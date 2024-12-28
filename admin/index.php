885dcd724129d22e38613bb82729c32d0ffc9982
yukyhendiawan <yukyhendiawan123@gmail.com>
2024-12-29 01:37:41 +0700

    refactor: file index.php

--- admin/index.php
+++ admin/index.php
@@ -1,12 +1,14 @@
 <?php
 /**
- * Admin Index File.
+ * The index.php file.
+ *
+ * This file is intentionally left blank to prevent direct access to the directory.
  *
  * @link       https://yukyhendiawan.com
  * @since      1.0.0
  *
- * @package    Import_Export_Menu
- * @subpackage Import_Export_Menu/admin
+ * @package    Disable_Widgets_Block
+ * @subpackage Disable_Widgets_Block/includes
  */
 
 // Silence is golden.
